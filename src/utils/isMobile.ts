c8581293a2eec5c13a7dca22a8eec1694b526abb
Henry Heino <personalizedrefrigerator@gmail.com>
2024-09-20 13:24:27 -0700

    fix: Support older browser versions (fixes #1)

--- src/utils/isMobile.ts
+++ src/utils/isMobile.ts
@@ -1,5 +1,10 @@
 import joplin from "api"
 
 export const isMobile = async () => {
-	return (await joplin.versionInfo()).platform === 'mobile';
+	try {
+		return (await joplin.versionInfo()).platform === 'mobile';
+	} catch (e) {
+		console.warn('Error checking if on mobile:', e);
+		return false;
+	}
 }
